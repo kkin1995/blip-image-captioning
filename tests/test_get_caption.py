ac3c428294ca5c61d971847c5501142e21d441d3
kkin1995 <7694983+kkin1995@users.noreply.github.com>
2023-03-17 12:46:40 +0530

    Removed test for raised warning. Added tests to check return type.

--- tests/test_get_caption.py
+++ tests/test_get_caption.py
@@ -10,19 +10,23 @@ class TestGetImageCaptionFromModel:
 
     def test_string_output(self, path_to_image):
         image = Image.open(path_to_image)
-        caption = get_image_caption_from_model(image, None)
+        caption = get_image_caption_from_model(image)
 
-        assert type(caption) == str
-
-    def test_raised_warning(self, path_to_image):
-        image = Image.open(path_to_image)
-        with pytest.warns(UserWarning):
-            _ = get_image_caption_from_model(image, None)
+        assert type(caption) == list
+        assert type(caption[0]) == str
 
     def test_conditional_image_caption(self, path_to_image):
         image = Image.open(path_to_image)
         text = "A man running"
-        caption = get_image_caption_from_model(image, None, text=text)
-        index = caption.find(text.lower())
+        caption = get_image_caption_from_model(image, text=text)
+        index = caption[0].find(text.lower())
 
         assert index != -1
+
+    def test_conditional_image_caption_with_list(self, path_to_image):
+        image = Image.open(path_to_image)
+        text = ["A man", "A guy", "Football"]
+        caption = get_image_caption_from_model(image, text=text)
+
+        assert type(caption) == list
+        assert len(caption) == 3
